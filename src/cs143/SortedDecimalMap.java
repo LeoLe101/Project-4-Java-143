436e1a2925256c3d81b39b00968fd79221782c33
958016796 <958016796@LYN301-010.acs.edcc.ctc.edu>
2017-05-18 13:23:14 -0700

    Finish the fillQueue() in the Decimal Iterator

--- src/cs143/SortedDecimalMap.java
+++ src/cs143/SortedDecimalMap.java
@@ -5,9 +5,9 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 /**
- * This is the concrete class that will create the Sorted Decimal Map by 
+ * This is the concrete class that will create the Sorted Decimal Map by
  * implementing the Decimal Search Tree
- * 
+ *
  * @author Phuc Hong Le
  * @version 5/17/2017
  * @param <E> generic type that will decided the object within the tree
@@ -21,9 +21,9 @@ public class SortedDecimalMap<E extends DecimalSortable>
 
     /**
      * Constructor.
-     * 
-     * @param digitCount the largest number of digits a sorting key will
-     * contain in this sorted decimal map
+     *
+     * @param digitCount the largest number of digits a sorting key will contain
+     * in this sorted decimal map
      */
     public SortedDecimalMap(int digitCount) {
         root = new DecimalNode();
@@ -32,19 +32,19 @@ public class SortedDecimalMap<E extends DecimalSortable>
 
     /**
      * Check if the tree contains the value by its key number
-     * 
+     *
      * @param key the key number attached with the value inside of the tree
-     * @return true if a value with given key exists, false if it is not 
+     * @return true if a value with given key exists, false if it is not
      */
     @Override
     public boolean contains(int key) {
-        
+
         return true;
     }
 
     /**
-     * Get the value with the given key number 
-     * 
+     * Get the value with the given key number
+     *
      * @param key the key number attached with the value inside of the tree
      * @return the value of its given key, null if the value does not exist
      */
@@ -55,12 +55,12 @@ public class SortedDecimalMap<E extends DecimalSortable>
     }
 
     /**
-     * Insert the value which has a unique non-negative whole-number key 
-     * to the tree
-     * 
+     * Insert the value which has a unique non-negative whole-number key to the
+     * tree
+     *
      * @param e the value to be inserted
      * @return true if the value has been inserted, false if is not
-     */ 
+     */
     @Override
     public boolean insert(E e) {
         // TODO -- write this code
@@ -69,7 +69,7 @@ public class SortedDecimalMap<E extends DecimalSortable>
 
     /**
      * Remove the value with the given key from the tree
-     * 
+     *
      * @param key the key number attached with the value inside of the tree
      * @return true if the value is found and removed, false if it is not
      */
@@ -81,8 +81,8 @@ public class SortedDecimalMap<E extends DecimalSortable>
 
     /**
      * Reports if the tree is empty or not.
-     * 
-     * @return true if the tree is empty, false if not 
+     *
+     * @return true if the tree is empty, false if not
      */
     @Override
     public boolean isEmpty() {
@@ -91,18 +91,18 @@ public class SortedDecimalMap<E extends DecimalSortable>
 
     /**
      * Private recursive method to determine if the tree is empty.
-     * 
+     *
      * @param node the current node the recursion is on
      * @return false if a value is found in the tree, true if recursion is
      * complete and no values were found
      */
     private boolean isEmpty(DecimalNode node) {
         for (int i = 0; i < 10; i++) {
-            if (node.children[i] != null) {
-                if (node.children[i].value != null) {
+            if (node.children[i] != null) { //Check if the current node have any children node in the children[] or not
+                if (node.children[i].value != null) { //Check if the current node have any value or not
                     return false;
                 } else {
-                    return isEmpty(node.children[i]);
+                    return isEmpty(node.children[i]); //recursively do the method again with the children node of the current node
                 }
             }
         }
@@ -111,8 +111,8 @@ public class SortedDecimalMap<E extends DecimalSortable>
 
     /**
      * Provides access to a type-specific iterator.
-     * 
-     * @return a new iterator object 
+     *
+     * @return a new iterator object
      */
     @Override
     public Iterator<E> iterator() {
@@ -145,19 +145,31 @@ public class SortedDecimalMap<E extends DecimalSortable>
         }
 
         /**
-         * A private recursive method to fill the queue with the
-         * value of each node in sorted order.
-         * 
+         * A private recursive method to fill the queue with the value of each
+         * node in sorted order.
+         *
          * @param node the current node in the recursive process
          */
         private void fillQueue(DecimalNode node) {
-            // TODO -- write this code
+            for (int i = 0; i < 10; i++) {
+                //check if the children of the current Node is null or not
+                if (node.children[i] != null) {
+                    //check if that children Node has a leaf Node or not
+                    if (node.children[i].value != null) {
+                        //if the Node has a value, add it in the queue
+                        queue.add((E)node.children[i].value);
+                    } else {
+                        //if not, restart the method using the current Node's childrenNode
+                        fillQueue(node.children[i]);
+                    }
+                }
+            }
         }
 
         /**
          * Determines if there is a next value in the map.
-         * 
-         * @return true if there is a next value, false if not 
+         *
+         * @return true if there is a next value, false if not
          */
         @Override
         public boolean hasNext() {
@@ -169,7 +181,7 @@ public class SortedDecimalMap<E extends DecimalSortable>
 
         /**
          * Provides access to the next value in the map, in sorted order.
-         * 
+         *
          * @return the next value
          */
         @Override
@@ -193,11 +205,11 @@ public class SortedDecimalMap<E extends DecimalSortable>
     private class DecimalNode<E> {
 
         /**
-         * the value stored in this node - will be null for all nodes except
-         * the lowest-level leaf nodes
+         * the value stored in this node - will be null for all nodes except the
+         * lowest-level leaf nodes
          */
         public E value;
-        
+
         /**
          * the array used to store the children of this node
          */
@@ -211,11 +223,11 @@ public class SortedDecimalMap<E extends DecimalSortable>
         }
 
         /**
-         * A convenience method to create a new node at the given
-         * index of the current node. 
-         * 
-         * @param index the index of the children array where the new
-         * node should be stored
+         * A convenience method to create a new node at the given index of the
+         * current node.
+         *
+         * @param index the index of the children array where the new node
+         * should be stored
          */
         public void makeChild(int index) {
             children[index] = new DecimalNode();
